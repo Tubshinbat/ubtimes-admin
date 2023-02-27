771be173916e302559295b510d1b339ad277c58b
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-02-28 00:21:38 +0800

    a

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://admin.zaya-ananda.com/api/",
-  baseURL: "http://localhost:8090/api/v1/",
+  baseURL: "http://ubtimes-admin.metaldoor.mn/api/",
 });
 
 instance.defaults.withCredentials = true;
